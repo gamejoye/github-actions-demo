867353369dddf8979ec3cb085651c43b6810f9e4
gamejoye <gamejoye@gmail.com>
2024-04-22 20:55:49 +0800

    xx

--- tests/mysql.js
+++ tests/mysql.js
@@ -4,7 +4,7 @@ const pc = require('child_process');
 const sqlFilePath = path.join(__dirname, './testdb.sql');
 
 pc.exec(
-  `mysql -u root -prootpassword testdb < ${sqlFilePath}`,
+  `mysql -h 127.0.0.1 -u root -prootpassword testdb < ${sqlFilePath}`,
   (error, stdout, stderr) => {
     console.log(error, stdout, stderr);
   }
