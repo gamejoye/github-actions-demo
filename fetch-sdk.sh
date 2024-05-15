01d3b795d8824a76a038893fb2926c3151f05b56
gamejoye <gamejoye@gmail.com>
2024-05-15 11:49:29 +0800

    test fetch

--- fetch-sdk.sh
+++ fetch-sdk.sh
@@ -1,24 +1,25 @@
 #!/bin/zsh
 
+# BACKEND_REPO="git@github.com:gamejoye/github-actions-demo.git"
 BACKEND_REPO="git@github.com:gamejoye/nestjs-imjoye.git"
 FRONTEND_REPO_DIR="./"
-TEMP_DIR="/tmp/backend-repo"
+TEMP_DIR="./tmp/backend-repo"
 SDK_DIR="sdk"
 
 git clone $BACKEND_REPO $TEMP_DIR
 
-rm -rf $FRONTEND_REPO_DIR/$SDK_DIR
+# rm -rf $FRONTEND_REPO_DIR/$SDK_DIR
 
-cp -R $TEMP_DIR/$SDK_DIR $FRONTEND_REPO_DIR/
+# cp -R $TEMP_DIR/$SDK_DIR $FRONTEND_REPO_DIR/
 
-cd $FRONTEND_REPO_DIR
+# cd $FRONTEND_REPO_DIR
 
-git add .
+# git add .
 
-git commit -m "chore(sdk): update sdk from server repo"
+# git commit -m "chore(sdk): update sdk from server repo"
 
-git push
+# git push
 
-rm -rf $TEMP_DIR
+# rm -rf $TEMP_DIR
 
 echo "SDK更新完成并已提交到前端仓库。"
